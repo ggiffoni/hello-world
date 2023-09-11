3cc4fd8baad4ae29937bc40d9451b7604b6e6925
ggiffoni <ggiffoni@gmail.com>
2023-09-11 16:03:39 -0300

    N4 criado no Ubuntu. Heranca.

---
+++ Conta Especial.py
@@ -0,0 +1,15 @@
+from Contas import Conta
+import datetime
+
+class ContaEspecial(Conta):
+    def __init__(self, clientes, numero, saldo, limite):
+        Conta.__init__(self, clientes, numero, saldo)
+        self.limite = limite
+
+    def sacar(self, valor):
+        if (self.saldo + self.limite) < valor:
+            return False
+        else:
+            self.saldo -= valor
+            self.extrato.transacoes.append(["SAQUE", valor, "DATA", datetime.datetime.today()])
+            return True
